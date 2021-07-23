6ae99c8025fe945186796b279e235ccd71146bee
antebm <ante@antebm.xyz>
2021-07-23 20:15:53 +0200

    inital commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.routing import request_response
+
+from services import blogpost_service
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def index(request: Request):
+    recent_posts = blogpost_service.read_recent()
+    return templates.TemplateResponse("index.html", {"request": request, "recent_posts": recent_posts})
+
+@app.get("/posts/{post_id}", response_class=HTMLResponse)
+async def read_post(request: Request, post_id: str):
+    blogpost = blogpost_service.read_blogpost(post_id)
+    return templates.TemplateResponse("post.html", {"request": request, "blogpost": blogpost})
---
+++ services/blogpost_service.py
@@ -0,0 +1,27 @@
+recent_blogposts = [
+    {
+        "title": "Title 1",
+        "author": "Author 1",
+        "content": "Content 1"
+    },
+    {
+        "title": "Title 2",
+        "author": "Author 2",
+        "content": "Content 2"
+    },
+    {
+        "title": "Title 3",
+        "author": "Author 3",
+        "content": "Content 3"
+    }
+]
+
+def read_blogpost(post_id: str):
+    return {
+        "title": "Title 1",
+        "author": "Author 1",
+        "content": "Content 1"
+    }
+
+def read_recent():
+    return recent_blogposts
